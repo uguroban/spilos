375f9c2fb3b8da0a6ee43698b4416266eaf445fd
uguroban <36131157+uguroban@users.noreply.github.com>
2022-09-12 21:54:32 +0300

    admin panel updated

--- Spilos/Controllers/AdminController.cs
+++ Spilos/Controllers/AdminController.cs
@@ -1,10 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Spilos.Controllers
 {
     public class AdminController : Controller
     {
+       
+
+        private readonly IWebHostEnvironment _environment;
+
+        public AdminController(IWebHostEnvironment environment)
+        {
+            _environment = environment; 
+        }
+
+        [Route("ManagementSpilosPanel")]
         public IActionResult ManagementSpilosPanel()
+        {
+            return View();
+        }
+
+        public IActionResult UploadPhoto()
         {
             return View();
         }
